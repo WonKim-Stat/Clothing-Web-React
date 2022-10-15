d99294e595b141ecf079d3c1f4ecb9cf15ae560c
WonKim-Stat <wonseokkim3@gmail.com>
2022-10-15 08:13:39 -0400

    Review until 114

--- src/components/routes/shop/shop.component.jsx
+++ src/components/routes/shop/shop.component.jsx
@@ -1,5 +1,18 @@
+import { useContext } from "react";
+import { ProductsContext } from "../../../contexts/products.context";
+import ProductCard from "../../product-card/product-card.component";
+
+import "./shop.styles.scss";
+
 const Shop = () => {
-  return <div>this is Shop page</div>;
+  const { products } = useContext(ProductsContext);
+  return (
+    <div className="products-container">
+      {products.map((product) => (
+        <ProductCard product={product} key={product.id} />
+      ))}
+    </div>
+  );
 };
 
 export default Shop;
