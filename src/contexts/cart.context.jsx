b0345cfa101ed9a3ba0ff4259c488f87002eaea8
WonKim-Stat <wonseokkim3@gmail.com>
2022-10-16 11:41:11 -0400

    completed until 118 context part2

--- src/contexts/cart.context.jsx
+++ src/contexts/cart.context.jsx
@@ -1,14 +1,42 @@
 // default form
 import { createContext, useState } from "react";
 
+// help function
+const identifyHandler = (cartItems, productToAdd) => {
+  const itemToAdd = cartItems.find((item) => item.id === productToAdd.id);
+  if (itemToAdd) {
+    return cartItems.map((cartItem) =>
+      cartItem.id === productToAdd.id
+        ? { ...cartItem, quantity: cartItem.quantity + 1 }
+        : cartItem
+    );
+  }
+
+  return [...cartItems, { ...productToAdd, quantity: 1 }];
+};
+
 export const CartContext = createContext({
   isCartOpen: false,
   setIsCartOpen: () => {},
+  cartItems: [],
+  setcartItems: () => {},
+  addItemToCart: () => {},
 });
 
 export const CartProvider = ({ children }) => {
   const [isCartOpen, setIsCartOpen] = useState(false);
-  const value = { isCartOpen, setIsCartOpen };
+  const [cartItems, setCartItems] = useState([]);
+
+  const addItemToCart = (productToAdd) => {
+    setCartItems(identifyHandler(cartItems, productToAdd));
+  };
+
+  const value = {
+    isCartOpen,
+    setIsCartOpen,
+    cartItems,
+    addItemToCart,
+  };
 
   return <CartContext.Provider value={value}>{children}</CartContext.Provider>;
 };
